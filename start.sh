9e29a9b14eac80b68ef0db22619de7afc210825a
Eric Rosenberg <ehaydenr@gmail.com>
2015-10-25 20:09:53 -0500

    Only restore 'classes' collection

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-if [ $1 == "scrape" ]
+if [ "$1" == "scrape" ]
 then
   /go/bin/scrape mongo
 else
   curl --insecure https://www.scheedule.com/dump.tar.gz | tar xz --
-  mongorestore --host mongo
+  mongorestore --host mongo --collection classes
 fi
 
 /go/bin/serve
