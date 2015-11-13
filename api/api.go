65503151c4df3dd28ec476c7e3ce0b52c93dcbac
Eric Rosenberg <ehaydenr@gmail.com>
2015-11-12 20:43:38 -0600

    Consolidate api package to be more concise. Additional request tests added.

--- api/api.go
+++ api/api.go
@@ -3,7 +3,14 @@
 package api
 
 import (
+	"compress/gzip"
+	"encoding/json"
 	"errors"
+	"net/http"
+	"regexp"
+	"strconv"
+
+	log "github.com/Sirupsen/logrus"
 
 	"github.com/scheedule/coursestore/db"
 )
@@ -12,6 +19,11 @@ var (
 	BadRequestError = errors.New("The request was malformed.")
 	DBError         = errors.New("Query to database failed.")
 	UnmarshalError  = errors.New("Error unmarshalling data from the database.")
+	errorMap        = map[error]int{
+		BadRequestError: http.StatusBadRequest,
+		DBError:         http.StatusNotFound,
+		UnmarshalError:  http.StatusInternalServerError,
+	}
 )
 
 // Type API contains the database to query and functions we use to query it.
@@ -21,4 +33,94 @@ type API struct {
 
 func New(db *db.DB) *API {
 	return &API{db}
+}
+
+// This route handles all requests to lookup individual class data. Requests
+// will have a department and number and class data will be returned as JSON.
+func (a *API) HandleLookup(w http.ResponseWriter, r *http.Request) {
+	department := r.FormValue("department")
+	number := r.FormValue("number")
+
+	if !isValidDepartment(department) || !isValidCourseNumber(number) {
+		log.Debug("query does not contain properly formatted dept/num combination.", BadRequestError)
+		handleError(w, BadRequestError)
+		return
+	}
+
+	log.Debug("querying: ", department, number)
+
+	class, err := a.db.Lookup(department, number)
+	if err != nil {
+		log.Warn("DB lookup failed: ", err)
+		handleError(w, DBError)
+		return
+	}
+
+	js, err := json.Marshal(class)
+	if err != nil {
+		log.Error("class unmarshal failed: ", err)
+		handleError(w, UnmarshalError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+// This route handles requests to get all the class data for every class in one
+// request. Data is returned as JSON.
+func (a *API) HandleAll(w http.ResponseWriter, r *http.Request) {
+	classes, err := a.db.GetAll()
+	if err != nil {
+		log.Error("failed to query all classes: ", err)
+		handleError(w, DBError)
+		return
+	}
+
+	js, err := json.Marshal(classes)
+	if err != nil {
+		log.Error("failed to unmarshal all classes: ", err)
+		handleError(w, UnmarshalError)
+		return
+	}
+
+	w.Header().Set("Content-Encoding", "gzip")
+	gz := gzip.NewWriter(w)
+	defer gz.Close()
+	gz.Write(js)
+}
+
+// Write the appropriate message to the client.
+func handleError(w http.ResponseWriter, err error) {
+	http.Error(w, err.Error(), errorMap[err])
+}
+
+// Return true if and only if the department is formatted correctly. This
+// function does not check the database for department existence.
+func isValidDepartment(department string) bool {
+	// Check empty
+	if department == "" {
+		return false
+	}
+
+	// Check capitalized alphabetic.
+	if matched, err := regexp.MatchString("^[A-Z]*$", department); !matched || err != nil {
+		return false
+	}
+
+	return true
+}
+
+func isValidCourseNumber(number string) bool {
+	// Check empty
+	if number == "" {
+		return false
+	}
+
+	// Check numeric
+	if _, err := strconv.Atoi(number); err != nil {
+		return false
+	}
+
+	return true
 }
--- api/api_test.go
+++ api/api_test.go
@@ -1,9 +1,16 @@
 package api
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"os"
+	"testing"
 
 	"github.com/scheedule/coursestore/db"
+	"github.com/scheedule/coursestore/types"
 )
 
 var testAPI *API
@@ -12,4 +19,104 @@ func init() {
 	testDB := db.New(os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"), os.Getenv("DB_COLLECTION"))
 	testDB.Init()
 	testAPI = New(testDB)
+}
+
+var departmentTests = []struct {
+	in  string
+	out bool
+}{
+	{"", false},
+	{"hello", false},
+	{"123", false},
+	{"CS", true},
+	{"AAS", true},
+}
+
+func TestIsValidDepartment(t *testing.T) {
+	for _, tt := range departmentTests {
+		result := isValidDepartment(tt.in)
+		if result != tt.out {
+			t.Fatalf("isValidDepartment(%q) => %q, want %q", tt.in, result, tt.out)
+		}
+	}
+}
+
+var courseNumberTests = []struct {
+	in  string
+	out bool
+}{
+	{"", false},
+	{"hello", false},
+	{"123", true},
+	{"1234", true},
+}
+
+func TestIsValidCourseNumber(t *testing.T) {
+	for _, tt := range courseNumberTests {
+		result := isValidCourseNumber(tt.in)
+		if result != tt.out {
+			t.Fatalf("isValidCourseNumber(%q) => %q, want %q", tt.in, result, tt.out)
+		}
+	}
+}
+
+var sampleClass = types.Class{
+	Department:   "CS",
+	CourseNumber: "125",
+}
+
+var classLookupTests = []struct {
+	department string
+	number     string
+	code       string
+}{
+	{"CS", "125", "200"},
+	{"CS", "225", "404"},
+}
+
+func TestLookup(t *testing.T) {
+	// Input class into database
+	testAPI.db.Purge()
+	err := testAPI.db.Put(sampleClass)
+	if err != nil {
+		t.Fatal("failed to put class in database: ", err)
+	}
+
+	for _, tt := range classLookupTests {
+		// Make HTTP Request
+		urlStr := fmt.Sprintf("/lookup?department=%s&number=%s", tt.department, tt.number)
+		req, err := http.NewRequest("GET", urlStr, nil)
+		if err != nil {
+			t.Fatal("failed to create request object.")
+		}
+
+		w := httptest.NewRecorder()
+		testAPI.HandleLookup(w, req)
+
+		code := fmt.Sprintf("%d", w.Code)
+		if code != tt.code {
+			t.Fatalf("response code received %q, want %q", code, tt.code)
+		}
+
+		if code != "200" {
+			return
+		}
+
+		data, err := ioutil.ReadAll(w.Body)
+		if err != nil {
+			t.Fatal("failed to read response.")
+		}
+		proposal := types.Class{}
+		err = json.Unmarshal(data, &proposal)
+		if err != nil {
+			t.Fatal("failed to decode response: ", err)
+		}
+
+		if proposal.Department != tt.department {
+			t.Fatalf("department contained %q, want %q", proposal.Department, tt.department)
+		}
+		if proposal.CourseNumber != tt.number {
+			t.Fatalf("course number contained %q, want %q", proposal.CourseNumber, tt.department)
+		}
+	}
 }
